9d8e9eec944d90e598de5093a6b1ad76ba875fcb
leehojin-93 <hogenie93@gmail.com>
2021-07-01 13:15:02 +0900

    21. 7. 1. Thu) 오전수업

---
+++ src/com/javaex/ex01/AuthorDeleteApp.java
@@ -0,0 +1,53 @@
+package com.javaex.ex01;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class AuthorDeleteApp {
+	public static void main(String[] args) {
+
+		// 0. import java.sql.*;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+//		ResultSet rs = null; // SELECT 문 불러올때 사용
+
+		try {
+			// 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			// 2. Connection 얻어오기
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "webdb", "webdb");
+			// 3. SQL문 준비 / 바인딩 / 실행
+			pstmt = conn.prepareStatement(" Delete FROM authors WHERE author_id = ? ");
+			pstmt.setInt(1, 7);
+
+			int count = pstmt.executeUpdate();
+
+			// 4. 결과처리
+			System.out.println(count + "건이 저장되었습니다.");
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+			System.out.println("error: " + e);
+		} finally {
+
+			// 5. 자원정리
+			try {
+				/*
+				 * if (rs != null) { rs.close(); }
+				 */
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("error: " + e);
+			}
+		}
+	}
+
+}
